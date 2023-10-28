e0820b4101a600c6b5f8f14a0dca2e45048517e8
Tar <mk1632@g.rit.edu>
2023-10-28 10:26:00 -0400

    Implemented SQL statement 17 and 19 in the same file

--- 17_19_follow_player.py
+++ 17_19_follow_player.py
@@ -14,11 +14,25 @@ def follow(following, follower):
         SELECT player_id
         FROM player
         WHERE username = '{follower}') )
-    
     """
     sql_command_2 = """"""
     print(execute_query(sql_command_1))
 
 
+def unfollow(following, follower):
+    sql_command = f"""
+    DELETE FROM follows
+    WHERE player_id = (
+        SELECT player_id
+        FROM player
+        WHERE username = '{following}' )
+    AND follower_id = (
+        SELECT player_id
+        FROM player
+        WHERE username = '{follower}' )
+    """
+    print(execute_query(sql_command))
+
+
 if __name__ == '__main__':
-    follow('nec', 'integer')
+    unfollow('nec', 'integer')
