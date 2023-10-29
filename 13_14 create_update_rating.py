f02f41f4f4253ace535773370a8e295eda73b2bf
Tar <mk1632@g.rit.edu>
2023-10-29 15:16:43 -0400

    Implemented SQL command 13, 14, 15, 16

---
+++ 13_14 create_update_rating.py
@@ -0,0 +1,37 @@
+from db_config import execute_query
+
+# Updates a player rating and creates a new rating entry if one does not
+# already exist for a game.
+def rate(player_id, game_id, rating):
+    sql_command_1 = f"""
+    SELECT player_id, game_id
+    FROM rates
+    WHERE player_id = {player_id} AND
+    game_id = {game_id}
+    """
+
+    sql_command_2 = f"""
+    INSERT INTO rates(player_id, game_id, rating)
+    VALUES({player_id}, {game_id}, {rating})
+    """
+
+    sql_command_3 = f"""
+    UPDATE rates
+    SET rating = {rating}
+    WHERE player_id = {player_id} AND
+    game_id = {game_id}
+    """
+
+    result = execute_query(sql_command_1)
+    print(result)
+
+    if not result:
+        print('DEBUG: Result was empty. Inserting new rating entry.')
+        print(execute_query(sql_command_2))
+    else:
+        print ('DEBUG: Result was not empty. Updating existing rating entry.')
+        print(execute_query(sql_command_3))
+
+
+if __name__ == '__main__':
+    rate(1597, 6, 4)
