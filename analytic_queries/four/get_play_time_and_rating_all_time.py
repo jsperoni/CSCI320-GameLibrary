916f110e43d6cdfce571dac9b63fede318d44ba4
Tar <mk1632@g.rit.edu>
2023-11-27 12:04:48 -0500

    Added new query to get play time and ratings of every game of all time.

---
+++ analytic_queries/four/get_play_time_and_rating_all_time.py
@@ -0,0 +1,13 @@
+from db_config import execute_query
+
+def get_all_play_time_and_rating():
+    query = ("""
+             SELECT p.game_id, SUM(EXTRACT(EPOCH FROM (end_time - start_time))) AS total_time, AVG(r.rating) AS average_rating
+             FROM plays p JOIN rates r on p.game_id = r.game_id
+             GROUP BY p.game_id
+             ORDER BY total_time DESC, average_rating DESC
+             """)
+    print(execute_query(query))
+
+if __name__ == '__main__':
+    get_all_play_time_and_rating()
